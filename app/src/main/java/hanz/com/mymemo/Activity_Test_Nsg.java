c86285f7fe8f4a262bed49c6d51fccc480a2ffc6
NSG-YEYING <1304374932@qq.com>
2020-06-16 11:16:26 +0800

    nsg

---
+++ app/src/main/java/hanz/com/mymemo/Activity_Test_Nsg.java
@@ -0,0 +1,13 @@
+package hanz.com.mymemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Test_Nsg extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__test__nsg);
+    }
+}
